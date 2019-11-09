9c29ff8d0eb3c447027063c4c4186f22f768784a
Raffaele Montella <raffaele.montella@uniparthenope.it>
2019-11-09 11:34:39 +0100

    Implemented Celery and Naive Queue

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
 from flask import Flask, Blueprint, url_for, jsonify, current_app, abort, send_from_directory
 from flask_restplus import Api, Resource, reqparse
-from flask_marshmallow import Marshmallow, base_fields
-from marshmallow import post_dump
+
 import werkzeug
 import tempfile
 
 import time
-import atexit
 
 
-from apscheduler.schedulers.background import BackgroundScheduler
+from celery import Celery
 
 import sys, os, shutil
 from os import listdir
 from os.path import isdir, isfile, join
 
-from uncompress import get_encoded_encrypted_symmetric_key, get_symmetric_key, uncrypt_update_list
-from store import store_updatelist_csv, store_updatelist_odbc
+from tasks import process_file_task, process_file_queue
+from queues import Queues
 
 import logging
 
-log = logging.getLogger('apscheduler.executors.default')
+log = logging.getLogger('app')
 log.setLevel(logging.DEBUG)  # DEBUG
 
 fmt = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
@@ -42,19 +40,19 @@ file_upload.add_argument('sessionId',
 app = Flask(__name__)
 
 
-
 class BaseConfig(object):
-    DATA_FOLDER="/data2/ccmmma/prometeo/data/dynamo-store/data_folder"
-    
-    MEDIA_ROOT   = DATA_FOLDER+"/media/"
-    SCRATCH_ROOT = DATA_FOLDER+"/scratch/"
-    TRASH_ROOT   = DATA_FOLDER+"/trash/"
-    CSV_ROOT     = DATA_FOLDER+"/csv/"
 
-    CONNECTION_STRING = "DRIVER=SQLite3;SERVER=localhost;DATABASE="+DATA_FOLDER+"/dynamo.db;Trusted_connection=yes"
+    MEDIA_ROOT = "data/media/"
+    SCRATCH_ROOT = "data/scratch/"
+    TRASH_ROOT = "data/trash/"
 
-    PRIVATE_KEY_FILENAME = "/home/ccmmma/dev/dynamo-store/keys/dynamo-store-private.pem"
-    PUBLIC_KEY_ROOT      = "/home/ccmmma/dev/dynamo-store/keys/public"
+    CONNECTION_STRING = "sqlite:///data/dynamo.db"
+
+    PRIVATE_KEY_FILENAME = "keys/dynamo-store-private.pem"
+    PUBLIC_KEY_ROOT = "keys/public"
+
+    CELERY_BROKER_URL = "pyamqp://guest@localhost//"
+    CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 
 config = {
@@ -68,79 +66,62 @@ def configure_app(app):
     app.config.from_pyfile('config.cfg', silent=True) # instance-folders configuration
 
 
-
 configure_app(app)
 
-scheduler = BackgroundScheduler()
-scheduler.start()
+celery = Celery(app.name, backend=app.config['CELERY_RESULT_BACKEND'], broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
-def heartbeat():
-    log.info("Start "+time.strftime("%A, %d. %B %Y %I:%M:%S %p")+"...")
-    job.pause()
-    log.debug("Paused")
 
+def myworker(queue_item):
+    print("myworker:" + queue_item)
+
+
+queues = Queues(process_file_queue, 8)
+queues.start()
+
+def get_conf():
     media_root = app.config.get("MEDIA_ROOT")
     scratch_root = app.config.get("SCRATCH_ROOT")
     trash_root = app.config.get("TRASH_ROOT")
-    csv_root = app.config.get("CSV_ROOT")
+
     connection_string = app.config.get("CONNECTION_STRING")
 
     private_key_filename = app.config.get("PRIVATE_KEY_FILENAME")
     public_key_root = app.config.get("PUBLIC_KEY_ROOT")
 
+    conf = {
+        "media_root": media_root,
+        "scratch_root": scratch_root,
+        "trash_root": trash_root,
+        "connection_string": connection_string,
+        "private_key_filename": private_key_filename,
+        "public_key_root": public_key_root
+    }
+
+    return conf
+
+
+def process_files():
+    log.info("Start "+time.strftime("%A, %d. %B %Y %I:%M:%S %p")+"...")
+
+    conf = get_conf()
+    media_root=conf["media_root"]
+
     vessels = [f for f in listdir(media_root) if isdir(join(media_root, f))]
     for vessel in vessels:
-        public_key_filename=public_key_root+"/"+vessel+"-public.pem"
         vessel_root=media_root+"/"+vessel
         files = [f for f in listdir(vessel_root) if isfile(join(vessel_root, f))]
-        for file in files:
-            src_path=vessel_root+"/"+file
-            log.info("Processing: "+src_path)
-            try:
-                os.mkdir(scratch_root+"/"+vessel)
-            except:
-                pass
+        for file_item in files:
+            log.info("Processing:"+media_root+","+vessel+","+file_item)
+            #process_file_task.delay(vessel, file_item, conf)
+            queues.enqueue({"vessel": vessel, "file_item": file_item, "conf": conf})
 
-            scratch_dir=tempfile.mkdtemp(dir=scratch_root+"/"+vessel)
-            enc_path = scratch_dir + "/" + file
+    queues.join()
+    log.info("... "+time.strftime("%A, %d. %B %Y %I:%M:%S %p")+" finish.")
 
-            try:
-                log.debug("Get Eencoded Encrypted Symmetric Key")
-                encoded_encrypted_symmetric_key=get_encoded_encrypted_symmetric_key(src_path,enc_path)
-
-                log.debug("Get Symmetric Key")
-                symmetric_key=get_symmetric_key(private_key_filename, encoded_encrypted_symmetric_key)
-
-                log.debug("Uncrypt Update List")
-                update_list=uncrypt_update_list(public_key_filename, symmetric_key, enc_path)
-            
-                #log.debug("Store Update List CSV")
-                #store_updatelist_csv(update_list, { "csv_root": csv_root} )
-
-                log.debug("Store Update List ODBC")
-                store_updatelist_odbc(update_list, { "connection_string": connection_string} )
-            except ValueError as e:
-                log.error("Exception:", e)
-
-            shutil.rmtree(scratch_dir)
-
-            try:
-                os.mkdir(trash_root+"/"+vessel)
-            except:
-                pass
-
-            os.rename(src_path, trash_root + "/"+vessel + "/" + file )
-
-    log.debug("Resume")
-    job.resume()
-    log.info("...finish.")
-
-job=scheduler.add_job(func=heartbeat, trigger="interval", seconds=60)
-
-# Shut down the scheduler when exiting the app
-atexit.register(lambda: scheduler.shutdown())
 
 api = Api(app)
+process_files()
 
 @api.route('/publickey')
 class public_key(Resource):
@@ -150,7 +131,7 @@ class public_key(Resource):
 @api.route('/upload/<selfId>')
 class my_file_upload(Resource):
     @api.expect(file_upload)
-    def post(self,selfId):
+    def post(self, selfId):
 
         args = file_upload.parse_args()
         log.debug(args['file'].mimetype)
@@ -168,6 +149,11 @@ class my_file_upload(Resource):
             log.debug("Path: "+str(file_path))
 
             args['file'].save(file_path)
+
+            conf = get_conf()
+
+            #task = process_file_task.delay(selfId, file_path, conf)
+            queues.enqueue({"vessel": selfId, "file_item": temp_name+'.log.gz.enc', "conf": conf})
         else:
             abort(404)
         return {'status': 'Done'}
