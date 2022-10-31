06170d3a8cc5d6143c74997f6edb6619dcb0d267
Raffaele Montella <raffaele.montella@uniparthenope.it>
2022-10-31 10:55:16 +0100

    New setup.py automatically generated by PyCharm

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dynamo-signalk-storage-server',
+    version='1.0.0',
+    packages=['app'],
+    url='http://fairwind.uniparthenope.it',
+    license='Apache 2.0',
+    author='The FairWind Team',
+    author_email='fairwind@uniparthenope.it',
+    description='DYNAMO Signa K Storage Server'
+)
