5833abdcb4b68d8191fa592d3070f3ef011200c7
Raffaele Montella <raffaele.montella@uniparthenope.it>
2022-10-31 11:01:39 +0100

    Changed http to https

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dynamo-signalk-storage-server',
     version='1.0.0',
     packages=['app'],
-    url='http://fairwind.uniparthenope.it',
+    url='https://fairwind.uniparthenope.it',
     license='Apache 2.0',
     author='The FairWind Team',
     author_email='fairwind@uniparthenope.it',
